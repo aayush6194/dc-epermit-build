21e785705ef692f5e955a6f812815258c8fbadf2
Aayush Shrestha <aayush6194@gmail.com>
2021-07-14 10:35:23 -0500

    done

--- client/src/pages/electric/map/index_v1.tsx
+++ client/src/pages/electric/map/index_v1.tsx
@@ -113,8 +113,8 @@ export class MapContainer extends React.Component<any, any> {
                 >
                   
                   <h2>Location: {"Largo Town Center, Largo, MD 20774"}</h2>
-                  <div># of Chargers: 12</div>
-                  <div>Occupancy: 50 %</div>
+                  <Text bold size='1.2em'> # of Chargers: 12</Text>
+                  <Text bold  size='1.2em'>Occupancy: 50 %</Text>
                 </div>
               </InfoWindowEx>
             </Map>
