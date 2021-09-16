74168c558e35290353901b333cbd64c2c0464a79
Aayush Shrestha <aayush6194@gmail.com>
2021-09-16 17:30:34 -0500

    empty text

--- client/src/config.ts
+++ client/src/config.ts
@@ -25,7 +25,7 @@ export const top2 = 1000;
 export const top1 = 10000;
 export const API = "https://test.findparkstash.com/api";
 export const url = 'https://standford-epermit.findparkstash.com'
-export const API2 = url  + "/api";
+export const API2 = (url)  + "/api";
 export const defaultLocation =  {lat: 39.02653927586001, lng: -76.95245338598109};
 export const token = 'jTVnIl0ahL'
 
--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
-export const PORT = 4488;
+export const PORT = 8902;
 export const mongodbUrl = 'mongodb://abhaya:password@35.222.67.246:27017/sjsu_client?replicaSet=rs0&authSource=admin';
